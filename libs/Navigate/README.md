18c5343588b807ed90c96f3283ffd3ea03a627de
Thuc Trinh <trinhhongthuc57@gmail.com>
2025-09-20 09:31:03 +0700

    [FEAUTURE] Init repo

---
+++ libs/Navigate/README.md
@@ -0,0 +1,7 @@
+# Navigate
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test Navigate` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/Navigate/src/lib/Navigate.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledNavigate = styled.div`
+  color: pink;
+`;
+export function Navigate() {
+  return (
+    <StyledNavigate>
+      <h1>Welcome to Navigate!</h1>
+    </StyledNavigate>
+  );
+}
+
+export default Navigate;
